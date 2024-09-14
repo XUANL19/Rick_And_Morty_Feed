b8dc33c90dd1c7e7fa25fcd86af59b8f9559aca6
Xuan Li <xl3125@columbia.edu>
2024-09-13 17:32:06 -0700

    another change

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Title: React.FC = () => {
-  return <h1 style={{ textAlign: 'center' }}>Rick and Morty Characters</h1>;
+  return <h1 style={{ textAlign: 'center' }}>Rick and Morty Characterssssaaa</h1>;
 };
 
 export default Title;
